41eefda3d3c94b05d4a389190ac7909e13d41198
Deepshikha Gangele <242769008+deepshikhagangele-svg@users.noreply.github.com>
2025-11-20 13:59:04 +0530

    Add CounterApp component to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import UserForm from "./components/UserForm";
+import CounterApp from "./components/CounterApp"
 
 function App() {
   return (
     <div>
+      <CounterApp/>
       <UserForm />
     </div>
   );
